cec4bc084ed21e9c2da3a0b947fac0e729cc374b
Tom <tomrgowan@gmail.com>
2024-06-12 17:17:25 +1000

    feat: swap with contract

--- apps/nft/front_end/tests/tests.rs
+++ apps/nft/front_end/tests/tests.rs
@@ -1,8 +1,8 @@
 use std::{collections::HashMap, path::PathBuf, process::Stdio};
 
 use essential_rest_client::EssentialClient;
-use essential_types::{intent::Intent, IntentAddress};
-use nft_front_end::Nft;
+use essential_types::{intent::Intent, ContentAddress, IntentAddress};
+use nft_front_end::{Addresses, Nft};
 use tokio::{
     fs::File,
     io::{AsyncBufReadExt, AsyncReadExt, BufReader},
@@ -61,24 +61,36 @@ async fn mint_and_transfer() {
 
     let server_address = format!("http://localhost:{}", port);
 
-    let auth_intents = compile_pint_file("auth.pnt").await;
-
-    let auth_addresses = named_addresses(&auth_intents, &["init", "key"]);
-    for (name, address) in &auth_addresses {
-        println!(
-            "{}: set: {}, intent: {}",
-            name,
-            hex::encode_upper(address.set.0),
-            hex::encode_upper(address.intent.0),
-        );
-    }
+    let key_intents = compile_pint_file("key.pnt").await;
+    let key_addresses = named_addresses(&key_intents, &["init", "key"]);
 
     let nft_intents = compile_pint_file("nft.pnt").await;
+    let nft_addresses = named_addresses(&nft_intents, &["mint", "transfer"]);
+
+    let auth_intents = compile_pint_file("auth.pnt").await;
+    let auth_addresses = named_addresses(&auth_intents, &["auth"]);
+
+    let swap_any_intents = compile_pint_file("swap_any.pnt").await;
+    let swap_any_addresses = named_addresses(&swap_any_intents, &["init", "swap"]);
+
+    let addresses = Addresses {
+        nft: nft_addresses["mint"].set.clone(),
+        nft_mint: nft_addresses["mint"].clone(),
+        nft_transfer: nft_addresses["transfer"].clone(),
+        auth: auth_addresses["auth"].set.clone(),
+        auth_auth: auth_addresses["auth"].clone(),
+        key: key_addresses["key"].set.clone(),
+        key_init: key_addresses["init"].clone(),
+        key_key: key_addresses["key"].clone(),
+        swap_any: swap_any_addresses["swap"].set.clone(),
+        swap_any_init: swap_any_addresses["init"].clone(),
+        swap_any_swap: swap_any_addresses["swap"].clone(),
+    };
+
+    print_addresses(&addresses);
 
     // Deploy intents
     let client = EssentialClient::new(server_address.clone()).unwrap();
-
-    let nft_addresses = named_addresses(&nft_intents, &["mint", "transfer"]);
 
     let mut wallet = essential_wallet::Wallet::temp().unwrap();
 
@@ -88,29 +100,27 @@ async fn mint_and_transfer() {
 
     let intents = wallet.sign_intent_set(nft_intents, "deployer").unwrap();
     client.deploy_intent_set(intents).await.unwrap();
+    let intents = wallet.sign_intent_set(key_intents, "deployer").unwrap();
+    client.deploy_intent_set(intents).await.unwrap();
     let intents = wallet.sign_intent_set(auth_intents, "deployer").unwrap();
+    client.deploy_intent_set(intents).await.unwrap();
+    let intents = wallet
+        .sign_intent_set(swap_any_intents, "deployer")
+        .unwrap();
     client.deploy_intent_set(intents).await.unwrap();
 
     let account_name = "alice";
 
     let art = "this_is_art";
-    let hash = essential_signer::hash_bytes(art.as_bytes()).unwrap();
+    let token = essential_signer::hash_bytes(art.as_bytes()).unwrap();
 
-    let mut nft = Nft::new(
-        server_address,
-        nft_addresses
-            .into_iter()
-            .chain(auth_addresses.into_iter())
-            .collect(),
-        wallet,
-    )
-    .unwrap();
+    let mut nft = Nft::new(server_address, addresses.clone(), wallet).unwrap();
 
     nft.create_account(account_name).ok();
 
-    nft.mint(account_name, hash).await.unwrap();
+    nft.mint(account_name, token).await.unwrap();
 
-    while !nft.do_i_own(account_name, hash).await.unwrap() {
+    while !nft.do_i_own(account_name, token).await.unwrap() {
         println!("I don't own the nft");
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
@@ -118,19 +128,78 @@ async fn mint_and_transfer() {
 
     let to = "bob";
     nft.create_account(to).ok();
-    nft.transfer(account_name, to, hash).await.unwrap();
+    nft.transfer(account_name, to, token).await.unwrap();
 
-    while nft.do_i_own(account_name, hash).await.unwrap() {
+    while nft.do_i_own(account_name, token).await.unwrap() {
         println!("I own the nft");
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
     println!("I don't the nft!!!");
 
-    while !nft.do_i_own(to, hash).await.unwrap() {
+    while !nft.do_i_own(to, token).await.unwrap() {
         println!("{} doesn't own the nft", to);
         tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     }
     println!("{} owns the nft!", to);
+
+    let diff_art = "different_art";
+    let art_token = essential_signer::hash_bytes(diff_art.as_bytes()).unwrap();
+
+    nft.mint_for_contract(&addresses.swap_any_swap, art_token)
+        .await
+        .unwrap();
+
+    while !nft
+        .do_i_own_contract(&addresses.swap_any_swap, art_token)
+        .await
+        .unwrap()
+    {
+        println!("Contract doesn't own the nft");
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+    println!("Contract owns the nft!!!");
+
+    nft.init_swap_any(art_token).await.unwrap();
+
+    loop {
+        if let Some(t) = nft.swap_any_owns().await.unwrap() {
+            if t == art_token {
+                println!("Contract initialized");
+                break;
+            }
+        }
+        println!("Contract not initialized");
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+
+    nft.swap_with_contract("bob", token).await.unwrap();
+
+    while nft.do_i_own("bob", art_token).await.unwrap() {
+        println!("Bob doesn't own different nft");
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+    println!("Bob owns different nft");
+
+    while !nft
+        .do_i_own_contract(&addresses.swap_any_swap, token)
+        .await
+        .unwrap()
+    {
+        println!("Contract doesn't own the nft");
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+    println!("Contract owns the original nft!!!");
+
+    loop {
+        if let Some(t) = nft.swap_any_owns().await.unwrap() {
+            if t == token {
+                println!("Contract state synced");
+                break;
+            }
+        }
+        println!("Contract state out of sync");
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
 }
 
 async fn compile_pint_file(name: &str) -> Vec<Intent> {
@@ -180,4 +249,44 @@ fn named_addresses(intents: &[Intent], names: &[&str]) -> HashMap<String, Intent
             )
         })
         .collect()
+}
+
+fn print_addresses(addresses: &Addresses) {
+    let Addresses {
+        nft,
+        nft_mint,
+        nft_transfer,
+        auth,
+        auth_auth,
+        key,
+        key_init,
+        key_key,
+        swap_any,
+        swap_any_init,
+        swap_any_swap,
+    } = addresses;
+    print_set_address("nft", nft);
+    print_address("nft_mint", nft_mint);
+    print_address("nft_transfer", nft_transfer);
+    print_set_address("auth", auth);
+    print_address("auth_auth", auth_auth);
+    print_set_address("key", key);
+    print_address("key_init", key_init);
+    print_address("key_key", key_key);
+    print_set_address("swap_any", swap_any);
+    print_address("swap_any_init", swap_any_init);
+    print_address("swap_any_swap", swap_any_swap);
+}
+
+fn print_address(name: &str, address: &IntentAddress) {
+    println!(
+        "{}: set: {}, intent: {}",
+        name,
+        hex::encode_upper(address.set.0),
+        hex::encode_upper(address.intent.0),
+    );
+}
+
+fn print_set_address(name: &str, address: &ContentAddress) {
+    println!("{}: set: {}", name, hex::encode_upper(address.0),);
 }
