770b98269522db6e69743a0c4aecd6a52ce5d331
Bikem <bengisuozaydin@gmail.com>
2024-06-16 18:07:01 -0400

    feat: token mint and transfer

---
+++ apps/token/app/src/inputs/token/mint.rs
@@ -0,0 +1,18 @@
+use crate::inputs::{Int, B256};
+use essential_types::Value;
+
+pub struct DecVars {
+    pub owner: B256,
+    pub amount: Int,
+}
+
+impl DecVars {
+    pub fn encode(&self) -> Vec<Value> {
+        self.owner
+            .to_value()
+            .into_iter()
+            .chain(self.amount.to_value())
+            .map(|w| vec![w])
+            .collect()
+    }
+}
