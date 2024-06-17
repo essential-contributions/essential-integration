2a24b275555397cde49c46a4e46f7bb06e7bd271
Bikem <bengisuozaydin@gmail.com>
2024-06-17 16:52:58 -0400

    feat: more app_utils and remaining CLI for the token app

--- apps/token/app/tests/tests.rs
+++ apps/token/app/tests/tests.rs
@@ -1,165 +1,38 @@
-// TODO: restructure
+use app_utils::local_server::setup_server;
+use std::path::PathBuf;
+use token::{actions::deploy_app, token::Token};
 
-use essential_rest_client::EssentialClient;
-use essential_types::{intent::Intent, ContentAddress, IntentAddress};
-use std::{collections::HashMap, path::PathBuf, process::Stdio};
-use token::token::{Addresses, Token};
-use tokio::{
-    fs::File,
-    io::{AsyncBufReadExt, AsyncReadExt, BufReader},
-    process::{Child, Command},
-};
-
-pub async fn compile(app_name: String) -> anyhow::Result<()> {
-    let pint_dir_path = concat!(env!("CARGO_MANIFEST_DIR"), "/../pint");
-    let pint_path = PathBuf::from(pint_dir_path).join(format!("{}.pnt", app_name));
-    anyhow::ensure!(pint_path.exists(), "failed to resolve pint dir path");
-    let pint_target_path = PathBuf::from(pint_dir_path).join("target");
-    if !pint_target_path.exists() {
-        std::fs::create_dir(&pint_target_path)
-            .map_err(|err| anyhow::anyhow!("failed to create pint target path: {}", err))?;
-        std::fs::create_dir(pint_dir_path).ok();
-    }
-    let output_path = pint_target_path.join(format!("{}.json", app_name));
-    let output = Command::new("pintc")
-        .arg(pint_path.display().to_string())
-        .arg("--output")
-        .arg(output_path.display().to_string())
-        .output()
-        .await
-        .map_err(|err| anyhow::anyhow!("failed to execute pint command: {}", err))?;
-    anyhow::ensure!(
-        output.status.success(),
-        "pintc failed: {}",
-        String::from_utf8_lossy(&output.stderr)
-    );
-    Ok(())
-}
-
-pub async fn read_output(
-    app_name: String,
-    intent_names: &[String],
-) -> anyhow::Result<(Vec<Intent>, HashMap<String, ContentAddress>)> {
-    let pint_target_path =
-        PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/../pint")).join("target");
-    let file = File::open(pint_target_path.join(format!("{}.json", app_name))).await?;
-    let mut bytes = Vec::new();
-    let mut reader = BufReader::new(file);
-    reader.read_to_end(&mut bytes).await?;
-    let intents: Vec<Intent> =
-        serde_json::from_slice(&bytes).expect("failed to deserialize intent set");
-    let intent_addresses = intent_names
-        .to_owned()
-        .into_iter()
-        .zip(intents.iter().map(essential_hash::content_addr))
-        .collect::<HashMap<String, _>>();
-    Ok((intents, intent_addresses))
-}
-
-pub async fn setup_server() -> anyhow::Result<(u16, Child)> {
-    let mut child = Command::new("essential-rest-server")
-        .env(
-            "RUST_LOG",
-            "[run_loop]=trace,[check_intent]=trace,[constraint]=trace,[recover_secp256k1]=trace",
-        )
-        .arg("--db")
-        .arg("memory")
-        .arg("0.0.0.0:0")
-        .arg("--loop-freq")
-        .arg("1")
-        .kill_on_drop(true)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    let stdout = child.stdout.take().unwrap();
-
-    let buf = BufReader::new(stdout);
-    let mut lines = buf.lines();
-
-    let port;
-    loop {
-        if let Some(line) = lines.next_line().await.unwrap() {
-            if line.contains("Listening") {
-                port = line
-                    .split(':')
-                    .next_back()
-                    .unwrap()
-                    .trim()
-                    .parse::<u16>()
-                    .unwrap();
-                break;
-            }
-        }
-    }
-
-    tokio::spawn(async move {
-        loop {
-            if let Some(line) = lines.next_line().await.unwrap() {
-                println!("{}", line);
-            }
-        }
-    });
-    assert_ne!(port, 0);
-    Ok((port, child))
-}
-
-pub async fn deploy_intents(
-    server_address: String,
-    wallet: &mut essential_wallet::Wallet,
-    account_name: &str,
-    intents: Vec<Intent>,
-    intent_addresses: HashMap<String, ContentAddress>,
-) -> anyhow::Result<Addresses> {
-    let client = EssentialClient::new(server_address)?;
-    let intents = wallet.sign_intent_set(intents, &account_name)?;
-    let set_address = client.deploy_intent_set(intents).await?;
-    let addresses = intent_addresses
-        .into_iter()
-        .map(|(n, i)| {
-            (
-                n,
-                IntentAddress {
-                    set: set_address.clone(),
-                    intent: i,
-                },
-            )
-        })
-        .collect::<HashMap<_, _>>();
-    Ok(Addresses {
-        token: set_address,
-        burn: addresses.get("burn").unwrap().clone(),
-        init: addresses.get("init").unwrap().clone(),
-        mint: addresses.get("mint").unwrap().clone(),
-        transfer: addresses.get("transfer").unwrap().clone(),
-    })
+#[tokio::test]
+#[ignore = "Will break CI because it requires the essential-rest-server to be on the path"]
+async fn mint_and_transfer_local() {
+    let (server_address, _child) = setup_server().await.unwrap();
+    mint_and_transfer(server_address).await;
 }
 
 #[tokio::test]
-async fn mint_and_transfer() {
-    let app_name = "token".to_string();
-    let intent_names = [
-        "burn".to_string(),
-        "init".to_string(),
-        "mint".to_string(),
-        "transfer".to_string(),
-    ];
-    let deployer_name = "deployer".to_string();
+#[ignore = "Will break CI because it runs on the deployed server."]
+async fn mint_and_transfer_remote() {
+    let server_address = std::env::var("ESSENTIAL_SERVER_ADDR").unwrap();
+    mint_and_transfer(server_address).await;
+}
+
+async fn mint_and_transfer(server_address: String) {
+    // setup essential wallet
     let mut wallet = essential_wallet::Wallet::temp().unwrap();
+
+    // setup deployer account
+    let deployer_name = "deployer".to_string();
     wallet
         .new_key_pair(&deployer_name, essential_wallet::Scheme::Secp256k1)
         .ok();
-    compile(app_name.clone()).await.unwrap();
-    let (intents, intent_addresses) = read_output(app_name, &intent_names).await.unwrap();
-    let (port, _child) = setup_server().await.unwrap(); // receiving child so that it is not dropped until the test is over
-    let server_address = format!("http://localhost:{}", port);
-    let intent_addresses = deploy_intents(
+
+    let pint_directory = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/../pint"));
+
+    let intent_addresses = deploy_app(
         server_address.clone(),
         &mut wallet,
         &deployer_name,
-        intents,
-        intent_addresses,
+        pint_directory,
     )
     .await
     .unwrap();
---
+++ apps/utils/src/local_server.rs
@@ -0,0 +1,56 @@
+use std::process::Stdio;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    process::{Child, Command},
+};
+
+pub async fn setup_server() -> anyhow::Result<(String, Child)> {
+    let mut child = Command::new("essential-rest-server")
+        .env(
+            "RUST_LOG",
+            "[run_loop]=trace,[check_intent]=trace,[constraint]=trace,[recover_secp256k1]=trace",
+        )
+        .arg("--db")
+        .arg("memory")
+        .arg("0.0.0.0:0")
+        .arg("--loop-freq")
+        .arg("1")
+        // this function returns `Child` so that it is not dropped prematurely despite this setting.        .kill_on_drop(true)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let stdout = child.stdout.take().unwrap();
+
+    let buf = BufReader::new(stdout);
+    let mut lines = buf.lines();
+
+    let port;
+    loop {
+        if let Some(line) = lines.next_line().await.unwrap() {
+            if line.contains("Listening") {
+                port = line
+                    .split(':')
+                    .next_back()
+                    .unwrap()
+                    .trim()
+                    .parse::<u16>()
+                    .unwrap();
+                break;
+            }
+        }
+    }
+
+    tokio::spawn(async move {
+        loop {
+            if let Some(line) = lines.next_line().await.unwrap() {
+                println!("{}", line);
+            }
+        }
+    });
+    assert_ne!(port, 0);
+
+    let server_address = format!("http://localhost:{}", port);
+    Ok((server_address, child))
+}
