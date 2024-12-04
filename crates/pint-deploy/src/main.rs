3cd152a66abab1c9ef8f4698cda8cb61bb60620b
Nick Furfaro <nfurfaro33@gmail.com>
2024-12-04 12:26:26 -0700

    chore: import pint-submit lib in pint deploy
    <!-- ps-id: 22b3beb8-1cdb-433d-ba4b-301cfd1b25d5 -->

--- crates/pint-deploy/src/main.rs
+++ crates/pint-deploy/src/main.rs
@@ -3,6 +3,7 @@ use clap::{builder::styling::Style, Parser};
 use essential_rest_client::builder_client::EssentialBuilderClient;
 use essential_types::{contract::Contract, ContentAddress};
 use pint_pkg::build::BuiltPkg;
+use pint_submit::SolutionInputType;
 use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug)]
