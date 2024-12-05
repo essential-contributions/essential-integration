36843966b63bff0225ec32db1dfb9ba033b68be3
Nick Furfaro <nfurfaro33@gmail.com>
2024-12-05 09:14:49 -0700

    refactor: move files into app-specific dirs

--- docs/code/counter/counter.rs
+++ docs/code/counter/counter.rs
@@ -1,47 +1,41 @@
-
 // ANCHOR: full
 // ANCHOR: use
 use anyhow::bail;
 use essential_types::{
     solution::{Mutation, Solution, SolutionData},
-    PredicateAddress, Value, Word,
+    Value, Word,
 };
 // ANCHOR_END: use
 
-// ANCHOR: key
-const COUNTER_KEY: Word = 0;
-// ANCHOR_END: key
-
-// ANCHOR: query
-#[derive(Clone)]
-pub struct QueryCount(pub Option<Value>);
-// ANCHOR_END: query
-
+// ANCHOR: abi_gen
+pint_abi::gen_from_file! {
+    abi: "../contract/out/debug/counter-abi.json",
+    contract: "../contract/out/debug/counter.json",
+}
+// ANCHOR_END: abi_gen
 
 // ANCHOR: counter-key
 #[derive(Clone)]
 pub struct CounterKey(pub Vec<Word>);
 
 pub fn counter_key() -> CounterKey {
-    CounterKey(vec![COUNTER_KEY])
+    let keys: Vec<_> = storage::keys().counter().into();
+    CounterKey(keys.first().unwrap().clone())
 }
 // ANCHOR_END: counter-key
 
 // ANCHOR: increment
-pub fn incremented_solution(
-    predicate: PredicateAddress,
-    count: QueryCount,
-) -> anyhow::Result<(Solution, Word)> {
+pub fn incremented_solution(count: Option<Value>) -> anyhow::Result<(Solution, Word)> {
     let count = extract_count(count)?;
     let new_count = count + 1;
-    Ok((create_solution(predicate, new_count), new_count))
+    Ok((create_solution(new_count), new_count))
 }
 // ANCHOR_END: increment
 
 // ANCHOR: extract
 /// Given a query of the current count, extract the count.
-pub fn extract_count(count: QueryCount) -> anyhow::Result<Word> {
-    match count.0 {
+pub fn extract_count(count: Option<Value>) -> anyhow::Result<Word> {
+    match count {
         Some(count) => match &count[..] {
             [] => Ok(0),
             [count] => Ok(*count),
@@ -53,16 +47,13 @@ pub fn extract_count(count: QueryCount) -> anyhow::Result<Word> {
 // ANCHOR_END: extract
 
 // ANCHOR: solution
-pub fn create_solution(predicate: PredicateAddress, new_count: Word) -> Solution {
+pub fn create_solution(new_count: Word) -> Solution {
+    let state_mutations: Vec<Mutation> = storage::mutations().counter(new_count).into();
     Solution {
         data: vec![SolutionData {
-            predicate_to_solve: predicate,
+            predicate_to_solve: Increment::ADDRESS,
             decision_variables: Default::default(),
-            transient_data: Default::default(),
-            state_mutations: vec![Mutation {
-                key: vec![COUNTER_KEY],
-                value: vec![new_count],
-            }],
+            state_mutations,
         }],
     }
 }
--- docs/code/counter/deploy.sh
+++ docs/code/counter/deploy.sh
@@ -17,5 +17,5 @@ cd ../
 
 # Deploy the contract
 # ANCHOR: deploy
-essential-rest-client deploy-contract "https://bigbangblock.builders" "contract/out/debug/counter.json"
+pint deploy --builder-address "https://node.essential.builders" --contract "contract/out/debug/counter.json"
 # ANCHOR_END: deploy
