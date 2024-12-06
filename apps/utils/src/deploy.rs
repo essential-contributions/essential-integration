176b7bc98f5fc3ae1198739370dd51210c9cf4a2
Nick Furfaro <nfurfaro33@gmail.com>
2024-12-06 10:04:34 -0700

    update apps utils deploy.rs
    <!-- ps-id: 25296e68-3483-4854-8b21-1d717a95f802 -->

--- apps/utils/src/deploy.rs
+++ apps/utils/src/deploy.rs
@@ -1,14 +1,14 @@
 use essential_node_types::register_contract_solution;
-use essential_types::{contract::Contract, solution::Solution, ContentAddress};
+use essential_types::{contract::Contract, solution::SolutionSet, ContentAddress};
 
 pub async fn deploy_contract(
     builder_conn: &essential_builder_db::ConnectionPool,
     contract: &Contract,
 ) -> anyhow::Result<ContentAddress> {
-    let solution = deploy_contract_solution(contract)?;
+    let solution_set = deploy_contract_solution(contract)?;
     let r = builder_conn
-        .insert_solution_submission(
-            std::sync::Arc::new(solution),
+        .insert_solution_set_submission(
+            std::sync::Arc::new(solution_set),
             std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .unwrap(),
@@ -17,10 +17,10 @@ pub async fn deploy_contract(
     Ok(r)
 }
 
-pub fn deploy_contract_solution(contract: &Contract) -> anyhow::Result<Solution> {
+pub fn deploy_contract_solution(contract: &Contract) -> anyhow::Result<SolutionSet> {
     let registry_predicate = essential_node_types::BigBang::default().contract_registry;
-    let solution = register_contract_solution(registry_predicate, contract)?;
-    Ok(Solution {
-        data: vec![solution],
+    let solution = register_contract_solution(registry_predicate.clone(), contract)?;
+    Ok(SolutionSet {
+        solutions: vec![solution],
     })
 }
